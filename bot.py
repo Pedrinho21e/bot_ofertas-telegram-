6e4f9e278c3a8c24106b5f54bfb7153b2eb4df64
Pedrinho21e <131800118+Pedrinho21e@users.noreply.github.com>
2026-02-16 10:59:17 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ from bs4 import BeautifulSoup
 # 1. CONFIGURAÇÕES
 TOKEN = '8579259563:AAEYxm0ktGMDBev2R2svYQ4nyV199CktzuA'
 CHAT_ID = '@promomilde'
-NOME_DA_PLANILHA = "Linkes,Status"
+NOME_DA_PLANILHA = "Ofertas"
 
 bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
@@ -80,6 +80,7 @@ if __name__ == "__main__":
     threading.Thread(target=lambda: app.run(host='0.0.0.0', port=8080)).start()
     threading.Thread(target=rodar_bot).start()
     bot.polling(none_stop=True)
+
 
 
 
